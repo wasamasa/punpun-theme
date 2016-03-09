f85b9ddd30a1fed07614f1ede1a349d3bbb101dc
Vasilij Schneidermann <v.schneidermann@gmail.com>
2016-03-09 21:08:21 +0100

    Add package header and footer

--- punpun-theme.el
+++ punpun-theme.el
@@ -1,3 +1,35 @@
+;;; punpun-theme.el --- A bleak theme.
+
+;; Copyright (C) 2016 Vasilij Schneidermann <v.schneidermann@gmail.com>
+
+;; Author: Vasilij Schneidermann <v.schneidermann@gmail.com>
+;; URL: https://github.com/wasamasa/punpun-theme
+;; Version: 0.0.1
+
+;; This file is NOT part of GNU Emacs.
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING. If not, write to
+;; the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
+;;; Commentary:
+
+;; A bleak theme.  Uses shades of gray and the occasional splash of
+;; color.  Designed for 256-color terminals.  Comes in light and dark!
+
+;;; Code:
+
 (defgroup punpun nil
   "A bleak theme"
   :group 'themes
@@ -108,3 +140,5 @@
                    (file-name-directory load-file-name))))
 
 (provide-theme 'punpun)
+
+;;; punpun-theme.el ends here
