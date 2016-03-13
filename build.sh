40296fedc0dc23f6b63b8b0839d44df091d0f35d
Vasilij Schneidermann <v.schneidermann@gmail.com>
2016-03-13 21:00:55 +0100

    Fix build.sh to actually work for Marmalade

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 version=$(cut -d\" -f4 punpun-theme-pkg.el)
-tar -cf punpun-theme-$version.tar punpun-common.el punpun-theme-pkg.el punpun-dark-theme.el punpun-light-theme.el
+basename="punpun-theme-$version"
+
+rm -rf $basename
+mkdir $basename
+cp punpun-common.el punpun-theme-pkg.el punpun-dark-theme.el punpun-light-theme.el LICENSE README.rst $basename
+tar -cf $basename.tar $basename
+rm -rf $basename
